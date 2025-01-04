4c7a25b3537802a52a4f975c0eefd7cdfb07dd38
Clarissa140 <mo432365@uaeh.edu.mx>
2025-01-03 19:34:00 -0600

    Diosayudame:/

--- src/components/SeccionP/Patrocinadores.jsx
+++ src/components/SeccionP/Patrocinadores.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import './Patrocinadores.css';
-
-import patron1 from '../../assets/PatrocinadoresHIMG/ALTARA.jpg';
-import patron2 from '../../assets/PatrocinadoresHIMG/Axelne.png';
-import patron3 from '../../assets/PatrocinadoresHIMG/CL.jpg';
-import patron4 from '../../assets/PatrocinadoresHIMG/CVM.jpg';
-import patron5 from '../../assets/PatrocinadoresHIMG/Donchon.jpg';
-import patron6 from '../../assets/PatrocinadoresHIMG/DrSalinas.jpg';
-import patron7 from '../../assets/PatrocinadoresHIMG/DrSalvador.jpg';
-import patron8 from '../../assets/PatrocinadoresHIMG/Frailes.png';
-import patron9 from '../../assets/PatrocinadoresHIMG/Mariscos.png';
-import patron10 from '../../assets/PatrocinadoresHIMG/PEIK.jpg';
-import patron11 from '../../assets/PatrocinadoresHIMG/logoimportane.jpg';
-import patron12 from '../../assets/PatrocinadoresHIMG/SraLeo.png';
-import patron13 from '../../assets/PatrocinadoresHIMG/Valle.png';
-import patron14 from '../../assets/PatrocinadoresHIMG/ahc.png';
-import patron15 from '../../assets/PatrocinadoresHIMG/ala.jpg';
-import patron16 from '../../assets/PatrocinadoresHIMG/daxi.png';
-import patron17 from '../../assets/PatrocinadoresHIMG/delatostada.png';
-import patron18 from '../../assets/PatrocinadoresHIMG/diamante.png';
-import patron19 from '../../assets/PatrocinadoresHIMG/diente.jpg';
-import patron20 from '../../assets/PatrocinadoresHIMG/kira.png';
-import patron21 from '../../assets/PatrocinadoresHIMG/sannicolas.png';
-import patron22 from '../../assets/PatrocinadoresHIMG/yolo.png';
+import patron1 from '../../assets/LogoPatrocinadores/LOGO1.png';
+import patron2 from '../../assets/LogoPatrocinadores/LOGO2.png';
+import patron3 from '../../assets/LogoPatrocinadores/LOGO3.png';
+import patron4 from '../../assets/LogoPatrocinadores/LOGO4.png';
+import patron5 from '../../assets/LogoPatrocinadores/LOGO5.png';
+import patron6 from '../../assets/LogoPatrocinadores/LOGO6.png';
+import patron7 from '../../assets/LogoPatrocinadores/LOGO7.png';
+import patron8 from '../../assets/LogoPatrocinadores/LOGO8.png';
+import patron9 from '../../assets/LogoPatrocinadores/LOGO9.png';
+import patron10 from '../../assets/LogoPatrocinadores/LOGO10.png';
+import patron11 from '../../assets/LogoPatrocinadores/LOGO11.png';
+import patron12 from '../../assets/LogoPatrocinadores/LOGO12.png';
+import patron13 from '../../assets/LogoPatrocinadores/LOGO13.png';
+import patron14 from '../../assets/LogoPatrocinadores/LOGO14.png';
+import patron15 from '../../assets/LogoPatrocinadores/LOGO15.png';
+import patron16 from '../../assets/LogoPatrocinadores/LOGO16.png';
+import patron17 from '../../assets/LogoPatrocinadores/LOGO17.png';
+import patron18 from '../../assets/LogoPatrocinadores/LOGO18.png';
+import patron19 from '../../assets/LogoPatrocinadores/LOGO19.png';
+import patron20 from '../../assets/LogoPatrocinadores/LOGO20.png';
+import patron21 from '../../assets/LogoPatrocinadores/LOGO21.png';
+import patron22 from '../../assets/LogoPatrocinadores/LOGO22.png';
 
 const sponsors = [
   patron1, patron2, patron3, patron4, patron5, patron6, patron7, patron8, patron9,
@@ -34,11 +33,11 @@ const Patrocinadores = () => {
   return (
     <div className='patrocinadores'>
       <div className='container'>
-        <h2>Patrocinadores Humildes</h2>
+        <h2>PATROCINADORES</h2>
         <div className='sponsors-grid'>
           {sponsors.map((sponsor, index) => (
             <div key={index} className='sponsor-item'>
-              <img src={sponsor} alt={`Patrocinador ${index + 1}`} />
+              <img src={sponsor} alt={'Patrocinador ${index + 1}'} />
             </div>
           ))}
         </div>
@@ -47,4 +46,4 @@ const Patrocinadores = () => {
   );
 };
 
-export default Patrocinadores;
+export default Patrocinadores;
